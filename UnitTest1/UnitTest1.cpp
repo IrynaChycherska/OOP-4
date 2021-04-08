23452dbefd9d7fd9ad8c0759421b675300b50855
unknown <iryna.chycherska.itis.2020@lpnu.ua>
2021-04-08 21:35:58 +0300

    4.2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../4.2/Arithmecic.h"
+#include"../4.2/Arithmecic.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Arithmecic z(1, 3, 2, 0);
+			Assert::IsTrue(z.Sum_progression() == (((z.GetN() + 1) * (z.GetA() + (z.GetA() + z.GetN() * z.GetD()))) / 2));
+		}
+	};
+}
